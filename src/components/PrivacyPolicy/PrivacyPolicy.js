75b24f43bccc222e4bfebd36dfda401cccc96f1c
Tatiana <tatimb14@gmail.com>
2019-06-27 17:15:59 -0600

    update static content (feature): terms of service and privacy policy page content updated

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -11,51 +11,171 @@ const PrivacyPolicy = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: October 30, 2017</p>
+      <p className={css.lastUpdated}>Effective as of April 03, 2019 </p>
 
       <p>
-        Thank you for using Saunatime! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Protecting your private information is our priority.
+        This Statement of Privacy applies to bolelo.org and
+        Askmyneighbor.org and governs data collection and usage.
+        For the purposes of this Privacy Policy, unless otherwise
+        noted, all references to Askmyneighbor.org include bolelo.org.
+        The bolelo.org website is a rental market place site. By using
+        the bolelo.org website, you consent to the data practices described in this statement.
       </p>
 
-      <h2>1 Lorem ipsum dolor sit amet</h2>
+      <h2>Collection of your Personal Information </h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        In order to better provide you with products and services
+         offered on our Site, bolelo.org may collect personally
+         identifiable information, such as your:
+      </p>
+      <ul>
+        <li>First and Last Name </li>
+        <li>Mailing Address  </li>
+        <li>E-mail Address  </li>
+        <li>Phone Number  </li>
+      </ul>
+      <p>If you purchase bolelo.org's services, we collect billing
+        and credit card information. This information is used to
+        complete the purchase transaction.
+      </p>
+      <p>Please keep in mind that if you directly disclose personally
+        identifiable information or personally sensitive data through
+        bolelo.org's public message boards, this information may be
+        collected and used by others. </p>
+      <p>
+        We do not collect any personal information about you unless you
+        voluntarily provide it to us. However, you may be required to
+        provide certain personal information to us when you elect to use
+        certain products or services available on the Site.
+        These may include: (a) registering for an account on our Site;
+        (b) entering a sweepstakes or contest sponsored by us or one of
+        our partners; (c) signing up for special offers from selected
+        third parties; (d) sending us an email message; (e) submitting
+        your credit card or other payment information when ordering and
+        purchasing products and services on our Site. To wit, we will use
+        your information for, but not limited to, communicating with you in
+        relation to services and/or products you have requested from us. We
+        also may gather additional personal or non-personal information in the future.
       </p>
 
-      <h2>2 Sed ut perspiciatis unde</h2>
+      <h2>Use of your Personal Information </h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        bolelo.org collects and uses your personal information to operate
+        its website(s) and deliver the services you have requested.
+      </p>
+      <p>
+        bolelo.org may also use your personally identifiable information
+        to inform you of other products or services available from bolelo.org and its affiliates.
       </p>
 
-      <h2>3 At vero eos et accusamus</h2>
+      <h2>Sharing Information with Third Parties </h2>
       <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat
+        bolelo.org may share data with trusted partners to help perform
+        statistical analysis, send you email or postal mail, provide customer
+         support, or arrange for deliveries. All such third parties are prohibited
+          from using your personal information except to provide these services to
+          bolelo.org, and they are required to maintain the confidentiality of your information.
       </p>
+      <p>
+        bolelo.org may disclose your personal information, without notice,
+        if required to do so by law or in the good faith belief that such
+        action is necessary to: (a) conform to the edicts of the law or
+        comply with legal process served on bolelo.org or the site; (b)
+        protect and defend the rights or property of bolelo.org; and/or
+        (c) act under exigent circumstances to protect the personal safety
+        of users of bolelo.org, or the public.
+      </p>
+      <h2>Tracking User Behavior  </h2>
+      <p>
+        bolelo.org may keep track of the websites and pages our users visit
+        within bolelo.org, in order to determine what bolelo.org services
+        are the most popular. This data is used to deliver customized content
+        and advertising within bolelo.org to customers whose behavior indicates
+        that they are interested in a particular subject area.
+      </p>
+      <h2>Automatically Collected Information </h2>
+      <p>
+        Information about your computer hardware and software may be automatically
+        collected by bolelo.org. This information can include your IP address, browser type,
+         domain names, access times and referring website addresses. This information is used
+         for the operation of the service, to maintain quality of the service, and to provide
+        general statistics regarding use of the bolelo.org website.
+      </p>
+      <h2>Use of Cookies </h2>
+      <p>
+        The bolelo.org website may use "cookies" to help you personalize your online experience.
+        A cookie is a text file that is placed on your hard disk by a web page server. Cookies
+        cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely
+        assigned to you, and can only be read by a web server in the domain that issued the cookie to you.
+      </p>
+      <p>
+        One of the primary purposes of cookies is to provide a convenience feature to save
+         you time. The purpose of a cookie is to tell the Web server that you have returned
+         to a specific page. For example, if you personalize bolelo.org pages, or register
+         with bolelo.org site or services, a cookie helps bolelo.org to recall your specific
+         information on subsequent visits. This simplifies the process of recording your personal
+         information, such as billing addresses, shipping addresses, and so on. When you return to
+         the same bolelo.org website, the information you previously provided can be retrieved, so
+         you can easily use the bolelo.org features that you customized.
+      </p>
+      <p>
+        You have the ability to accept or decline cookies. Most Web browsers automatically accept
+        cookies, but you can usually modify your browser setting to decline cookies if you prefer.
+        If you choose to decline cookies, you may not be able to fully experience the interactive
+        features of the bolelo.org services or websites you visit.
+      </p>
+      <h2>Security of your Personal Information </h2>
+      <p>
+        bolelo.org secures your personal information from unauthorized access, use, or disclosure.
+        bolelo.org uses the following methods for this purpose:
+      </p>
+      <ul>
+        <li>SSL Protocol</li>
+      </ul>
+      <p>When personal information (such as a credit card number) is transmitted to other websites,
+        it is protected through the use of encryption, such as the Secure Sockets Layer (SSL) protocol.
+      </p>
+      <p>
+        We strive to take appropriate security measures to protect against unauthorized access to
+        or alteration of your personal information. Unfortunately, no data transmission over the
+        Internet or any wireless network can be guaranteed to be 100% secure. As a result, while we
+         strive to protect your personal information, you acknowledge that: (a) there are security
+         and privacy limitations inherent to the Internet which are beyond our control; and (b)
+         security, integrity, and privacy of any and all information and data exchanged between you
+         and us through this Site cannot be guaranteed.
+      </p>
+      <h2>Children Under Eighteen </h2>
+      <p>
+        Bolelo.org does not knowingly collect personally identifiable information from children under
+         the age of eighteen. Due to safety concerns, financial and merchandise transaction requirement,
+         if you are under the age of eighteen, you may not use this site.
+      </p>
+      <h2>E-mail Communications </h2>
+      <p>
+        From time to time, bolelo.org may contact you via email for the purpose of providing announcements,
+        promotional offers, alerts, confirmations, surveys, and/or other general communication. In order to
+        improve our Services, we may receive a notification when you open an email from bolelo.org or click on a link therein.
+      </p>
+      <p>
+        If you would like to stop receiving marketing or promotional communications via email from bolelo.org,
+        you may opt out of such communications by Customers may unsubscribe from emails by "clicking on the UNSUBSCRIBE button.".
+      </p>
+      <h2>Changes to this Statement </h2>
+      <p>
+        bolelo.org reserves the right to change this Privacy Policy from time to time. We will notify you
+        about significant changes in the way we treat personal information by sending a notice to the primary
+        email address specified in your account, by placing a prominent notice on our site, and/or by updating
+        any privacy information on this page. Your continued use of the Site and/or Services available through
+        this Site after such modifications will constitute your: (a) acknowledgment of the modified Privacy Policy;
+        and (b) agreement to abide and be bound by that Policy.
+      </p>
+      <h2>Contact Information </h2>
+      <p>
+        bolelo.org welcomes your questions or comments regarding this Statement of Privacy. If you believe 
+        that bolelo.org has not adhered to this Statement, please contact bolelo.org at:
+      </p>
+      <span>Email Address:  bolelo.org@gmail.com </span>
     </div>
   );
 };

e083203df0cabc6585b1e9e4aacbd73f864249ec
Tatiana <tatimb14@gmail.com>
2019-07-05 13:30:26 -0600

    change the home page image to a carousel(fix): style adjustment

--- src/components/BackgroundCarousel/BackgroundCarousel.js
+++ src/components/BackgroundCarousel/BackgroundCarousel.js
@@ -1,6 +1,8 @@
 import React, { Component }  from 'react';
 import BackgroundSlider from 'react-background-slider';
 
+import css from './BackgroundCarousel.css';
+
 import image1 from '../../assets/carousel1.jpg';
 import image2 from '../../assets/carousel2.jpg';
 import image3 from '../../assets/carousel3.jpg';
@@ -14,7 +16,7 @@ import image8 from '../../assets/carousel8.jpg';
 const BackgroundCarousel = props => {
 
     return (
-        <div >
+        <div className={css.opacityImage}>
             <BackgroundSlider
                 images={[image1, image2, image3, image4, image5, image6, image7, image8]}
                 duration={8}

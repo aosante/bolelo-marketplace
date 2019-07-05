9e0566391f7a51f5376f0c7910eb499f52b8c6b6
Tatiana <tatimb14@gmail.com>
2019-07-05 11:13:06 -0600

    change the home page image to a carousel(feature): changing the home image to a carousel

---
+++ src/components/BackgroundCarousel/BackgroundCarousel.js
@@ -0,0 +1,29 @@
+import React, { Component }  from 'react';
+import BackgroundSlider from 'react-background-slider';
+
+import image1 from '../../assets/carousel1.jpg';
+import image2 from '../../assets/carousel2.jpg';
+import image3 from '../../assets/carousel3.jpg';
+import image4 from '../../assets/carousel4.jpg';
+import image5 from '../../assets/carousel5.jpg';
+import image6 from '../../assets/carousel6.jpg';
+import image7 from '../../assets/carousel7.jpg';
+import image8 from '../../assets/carousel8.jpg';
+
+
+const BackgroundCarousel = props => {
+
+    return (
+        <div >
+            <BackgroundSlider
+                images={[image1, image2, image3, image4, image5, image6, image7, image8]}
+                duration={8}
+                transition={2}
+            />
+        </div>
+    );
+};
+
+
+
+export default BackgroundCarousel;

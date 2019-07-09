95dc3eb066f18990ab6898fde36d1385f3e8a88a
aosante <andres.osante@proximitycr.com>
2019-07-09 13:38:15 -0600

    bg carousel ssr fix attempt

--- src/components/BackgroundCarousel/BackgroundCarousel.js
+++ src/components/BackgroundCarousel/BackgroundCarousel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import BackgroundSlideshow from 'react-background-slideshow';
 import 'react-multi-carousel/lib/styles.css';
@@ -11,13 +11,17 @@ import image6 from '../../assets/carousel6.jpg';
 import image7 from '../../assets/carousel7.jpg';
 import image8 from '../../assets/carousel8.jpg';
 
-const BackgroundCarousel = props => {
-  return (
-    <BackgroundSlideshow
-      images={[image1, image2, image3, image4, image5, image6, image7, image8]}
-      animationDelay={2000}
-    />
-  );
-};
+class BackgroundCarousel extends Component {
+  render() {
+    if (window !== 'undefined') {
+      return (
+        <BackgroundSlideshow
+          images={[image1, image2, image3, image4, image5, image6, image7, image8]}
+          animationDelay={6000}
+        />
+      );
+    }
+  }
+}
 
 export default BackgroundCarousel;

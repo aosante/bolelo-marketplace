9e615c9efff45d169abeaaf03c1911ea43b77da4
Tatiana <tatimb14@gmail.com>
2019-07-11 10:20:50 -0600

    feat(landPage): using bootstrap-react for the carousel

--- src/components/SectionHero/SectionHero.js
+++ src/components/SectionHero/SectionHero.js
@@ -3,7 +3,8 @@ import { string } from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { NamedLink } from '../../components';
 // import classNames from 'classnames';
-import Carousel from 'react-bootstrap';
+//import Carousel from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 
 import image1 from '../../assets/carousel1.jpg';
 import image2 from '../../assets/carousel2.jpg';
@@ -22,39 +23,56 @@ const SectionHero = props => {
   // const classes = classNames(rootClassName || css.root, className);
 
   return (
-    <div className="carousel slide" data-ride="carousel">
-      <div className="carousel-inner bg-container">
-        {/* heroContent div would be the equivalent to static text */}
-        <div className={css.heroContent}>
-          <h1 className={css.heroMainTitle}>
-            <FormattedMessage id="SectionHero.title" />
-          </h1>
-          <h2 className={css.heroSubTitle}>
-            <FormattedMessage id="SectionHero.subTitle" />
-          </h2>
-          <NamedLink
-            name="SearchPage"
-            to={{
-              search:
-                's?address=North%20Carolina%2C%20USA&bounds=36.5881568%2C-75.40011900000002%2C33.7528778%2C-84.32186899999999',
-            }}
-            className={css.heroButton}
-          >
-            <FormattedMessage id="SectionHero.browseButton" />
-          </NamedLink>
-        </div>
-        {/* hero content end */}
-        <div className="carousel-item active">
-          <img className={css.image} src={image1} alt="First slide" />
-        </div>
-        <div className="carousel-item">
-          <img className={css.image} src={image2} alt="Second slide" />
-        </div>
-        <div className="carousel-item">
-          <img className={css.image} src={image3} alt="Third slide" />
-        </div>
+    <div>
+      <div className={css.heroContent}>
+        <h1 className={css.heroMainTitle}>
+          <FormattedMessage id="SectionHero.title" />
+        </h1>
+        <h2 className={css.heroSubTitle}>
+          <FormattedMessage id="SectionHero.subTitle" />
+        </h2>
+        <NamedLink
+          name="SearchPage"
+          to={{
+            search:
+              's?address=North%20Carolina%2C%20USA&bounds=36.5881568%2C-75.40011900000002%2C33.7528778%2C-84.32186899999999',
+          }}
+          className={css.heroButton}
+        >
+          <FormattedMessage id="SectionHero.browseButton" />
+        </NamedLink>
       </div>
+      <Carousel controls={false} indicators={false}>
+        <Carousel.Item>
+          <img
+            className={css.image}
+            src={image1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className={css.image}
+            src={image2}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className={css.image}
+            src={image3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
     </div>
+
+
+
+
+
+
   );
 };
 

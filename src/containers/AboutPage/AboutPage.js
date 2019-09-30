5b9b17ad7ae18f8e0ff90cd74603bb685083c824
Jaime_Liu <jaime.liu@proximitycr.com>
2019-09-30 15:13:21 -0600

    Live fix

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -40,27 +40,32 @@ const AboutPage = () => {
 
             <div className={css.contentMain}>
               <h2>
-              So, what do we do with all the stuff we’ve bought but rarely use? Some of the items we have forgotten we even have.  We bought it because we needed it.  Now it sits in our garage, attic, shed, etc.
-How about that one item you really need but are not quite ready to make the purchase?
-BOLELO solves this problem with easy to use sharing platform by connecting potential Borrowers with people who are willing to Lend stuff they own.
-</h2>
+                So, what do we do with all the stuff we’ve bought but rarely use? Some of the items we have forgotten we even have.  We bought it because we needed it.  Now it sits in our garage, attic, shed, etc.
+                How about that one item you really need but are not quite ready to make the purchase?
+                BOLELO solves this problem with easy to use sharing platform by connecting potential Borrowers with people who are willing to Lend stuff they own.
+              </h2>
 
               <p>
-              Why buy when you can Borrow?<br/>
-Why buy when you only use it seldom and have no place to store it?<br/>
-If you are like many people we meet then you too have probably thought the same thing.
-</p>
-              <p>
-              Borrowing/Lending, however, is not so easy.  Often it’s hard to find a specific item you are looking for. We also know that goodwill Borrowing/Lending with friends and neighbors is not always ideal as it could result in ruining good relationships.  You will agree that a positive transaction with friendly strangers is much better than ruined relationships from a negative experience with friends.  With BOLELO, Borrowers save $$ and Lenders make $$.
+                Borrowing/Lending, however, is not so easy.  Often it’s hard to find a specific item you are looking for. We also know that goodwill Borrowing/Lending with friends and neighbors is not always ideal as it could result in ruining good relationships.  You will agree that a positive transaction with friendly strangers is much better than ruined relationships from a negative experience with friends.  With BOLELO, Borrowers save $$ and Lenders make $$.
               </p>
               <p>
+                Please send us your feedback and site improvement requests and we will do our best to meet your expectation.  Please keep in mind that we are a very small company.  We are so small that the sharks from the hit TV show Shark Tank probably would not even consider us a company, just a website 
+  Also, we hope that you will have patience with us as we learn and experience the growing pain while building the platform that meets your approval and creates an opportunity for you previously not available.
+  
+              </p>
+              <p>
+<<<<<<< HEAD
               Please send us your feedback and site improvement requests and we will do our best to meet your expectation.  Please keep in mind that we are a very small company.  We are so small that the sharks from the hit TV show Shark Tank probably would not even consider us a company, just a website <span role="img" aria-label="smiley-face">🙂</span>
 Also, we hope that you will have patience with us as we learn and experience the growing pain while building the platform that meets your approval and creates an opportunity for you previously not available.
 </p>
+=======
+                Our mission is simple.  We wish to make sharing a community thing by linking Borrowers and Lenders.  We dare to even say we are creating a better community and less wasteful environment that we can pass down to our children.  We hope you will share BOLELO with others.
+              </p>
 
-              <p >Our mission is simple.  We wish to make sharing a community thing by linking Borrowers and Lenders.  We dare to even say we are creating a better community and less wasteful environment that we can pass down to our children.  Together, let’s build a community that is socially, economically and environmentally sustainable. </p>
-              <p>We hope you will share BOLELO with others.  </p>
-              <p><em>BTW, we have chosen to donate 5% of proceeds to LB Side Kick Foundation (30-0162842), a 501 (c) 3, non-profit, based in Raleigh.  This is a small group of good people (100% volunteers) doing many great things for the community.</em> </p>
+              <p >BTW, we have chosen to donate 5% of proceeds to LB Side Kick Foundation (30-0162842), a 501 (c) 3, non-profit, based in Raleigh.  This is a small group of good people (100% volunteers) doing many great things for the community.     </p>
+
+>>>>>>> parent of 1c3ce9a9... Change to new email address,  add more information of insurance not covered items, about us and faq new format
+
             </div>
           </div>
         </LayoutWrapperMain>
